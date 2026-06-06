c2a60c16cbc960fbe6c74e27ded9a4e0ddfe0b7d
minh25800601139 <minh25800601139@hutech.edu.vn>
2026-06-06 20:12:24 +0700

    fix reloader

--- app.py
+++ app.py
@@ -284,4 +284,4 @@ def stream():
 
 if __name__ == "__main__":
     port = int(os.environ.get("PORT", 5000))
-    app.run(host="0.0.0.0", port=port, threaded=True)
+    app.run(host="0.0.0.0", port=port, threaded=True, use_reloader=False)
